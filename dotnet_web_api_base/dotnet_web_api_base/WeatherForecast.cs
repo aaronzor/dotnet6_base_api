2fb889ab557f47b3b0269ff0bd47c7038307a97e
aaronzor <mcmaaron@gmail.com>
2022-11-05 14:21:34 +0000

    Add files via upload

---
+++ dotnet_web_api_base/dotnet_web_api_base/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace dotnet_web_api_base
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
