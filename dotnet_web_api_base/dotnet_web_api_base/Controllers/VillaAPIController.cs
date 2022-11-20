ecfd46e2ec931fb463763a70c0a69fd32700a33d
Aaron McMullan <mcma@pop-os.localdomain>
2022-11-20 22:57:33 +0000

    add create villa endpoint

--- dotnet_web_api_base/dotnet_web_api_base/Controllers/VillaAPIController.cs
+++ dotnet_web_api_base/dotnet_web_api_base/Controllers/VillaAPIController.cs
@@ -15,8 +15,9 @@ namespace dotnet_web_api_base.Controllers
             return Ok(VillaStore.villaList);
         }
 
-        [HttpGet("{id:int}")]
-        [ProducesResponseType(200, Type =typeof(VillaDTO))]
+
+        [HttpGet("{id:int}", Name = "GetVilla")]
+        [ProducesResponseType(200, Type = typeof(VillaDTO))]
         [ProducesResponseType(404)]
         [ProducesResponseType(400)]
         public ActionResult<VillaDTO> GetVilla(int id)
@@ -26,6 +27,7 @@ namespace dotnet_web_api_base.Controllers
                 return BadRequest();
             }
 
+            // Villa Response Object
             var villa = VillaStore.villaList.FirstOrDefault(e => e.Id == id);
 
             if (villa == null)
@@ -33,8 +35,30 @@ namespace dotnet_web_api_base.Controllers
                 return NotFound();
             }
 
-
             return Ok(villa);
+        }
+
+        [HttpPost]
+
+        public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
+        {
+            // Model State is used if not using ApiController
+            // if (!ModelState.IsValid)
+            // {
+            //     return BadRequest(ModelState);
+            // }
+            if (villaDTO == null)
+            {
+                return BadRequest(villaDTO);
+            }
+            if (villaDTO.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+            VillaStore.villaList.Add(villaDTO);
+
+            return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
         }
     }
 }
--- dotnet_web_api_base/dotnet_web_api_base/Models/DTO/VillaDTO.cs
+++ dotnet_web_api_base/dotnet_web_api_base/Models/DTO/VillaDTO.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace dotnet_web_api_base.Models.DTO
 {
 
-  public class VillaDTO
-  {
-    public int Id { get; set; }
-    public string Name { get; set; }
-  }
+    public class VillaDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+    }
 }
