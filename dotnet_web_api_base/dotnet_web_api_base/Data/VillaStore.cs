67630144d9ab248f4c391da6a203d1d9fc1eef8a
Aaron McMullan <mcmaaron@gmail.com>
2022-11-22 18:54:02 +0000

    villaStore update

--- dotnet_web_api_base/dotnet_web_api_base/Data/VillaStore.cs
+++ dotnet_web_api_base/dotnet_web_api_base/Data/VillaStore.cs
@@ -3,11 +3,11 @@ using dotnet_web_api_base.Models.DTO;
 namespace dotnet_web_api_base.Data
 {
 
-  public static class VillaStore
-  {
-    public static List<VillaDTO> villaList = new List<VillaDTO> {
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO> {
       new VillaDTO{Id=1, Name="Pool View"},
       new VillaDTO{Id=2, Name="Beach View"}
-    }; 
-  }
+    };
+    }
 }
